2ccdfec86cccc9e36fc6f8e47adcd14af05f0573
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-06-08 10:24:39 +0700

    Ex2 SS8

--- Ex2.sql
+++ Ex2.sql
@@ -1,24 +1,25 @@
-create view v_order_summary as
-select full_name, total_amount,
-       order_date from orders
-inner join customer on orders.customer_id = customer.customer_id;
+create or replace procedure check_stock(
+    p_id INT,
+    p_qty INT
+) language plpgsql
+as $$
+    declare
+    v_quantity int;
+    begin
+        select quantity into v_quantity from inventory
+        where product_id=p_id;
+        if v_quantity is null then
+            raise notice 'Không tồn tại hàng';
+        elsif v_quantity >= p_qty then
+            raise notice 'Có đủ hàng';
+        else
+            raise notice 'Không đủ hàng';
+        end if;
+    exception
+        when others then
+            raise notice 'Xảy ra lỗi: %',SQLERRM;
+        rollback;
+    end;
+$$;
 
-select *from v_order_summary;
-
-create view v_order as
-select *from orders
-where total_amount>=1000000 with check option;
-
-select *from v_order;
-
-update v_order
-set total_amount=2300000 where id=2;
-
-create view v_monthly_sales as
-select distinct extract(month from order_date),sum(total_amount) from orders
-group by extract(month from order_date);
-
-drop view v_monthly_sales;
-
--- DROP VIEW chỉ xoá định nghĩa trong hệ thống, không xoá gì trong dữ liệu gốc
--- DROP MATERIALIZED VIEW xoá bảng MV thực sự, cả index của MV đó trên ổ cứng
+call check_stock(99,1000);
