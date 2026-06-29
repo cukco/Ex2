381580d4dacf318ae4d4c2d691e22645e2d985c8
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-06-29 11:40:45 +0700

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,13 +1,47 @@
 import java.util.Scanner;
-
-public class Ex2 {
+public class Ex22 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số thứ nhất (firstNumber): ");
-        int n1 = scanner.nextInt();
-        System.out.print("Nhập số thứ hai (secondNumber): ");
-        int n2 = scanner.nextInt();
-        System.out.println("--- Kết quả ---");
-        System.out.printf("firstNumber = %d\nsecondNumber = %d\nTổng = %d\nHiệu = %d\nTích = %d\nThương = %d\nPhần dư =%d",n1,n2,n1+n2,n1-n2,n1*n2,n1/n2,n1%n2);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        switch (n){
+            case 1:
+                System.out.printf("Tháng %d có 31 ngày",n);
+                break;
+            case 2:
+                System.out.printf("Tháng %d có 28 hoặc 29 ngày",n);
+                break;
+            case 3:
+                System.out.printf("Tháng %d có 31 ngày",n);
+                break;
+            case 4:
+                System.out.printf("Tháng %d có 30 ngày",n);
+                break;
+            case 5:
+                System.out.printf("Tháng %d có 31 ngày",n);
+                break;
+            case 6:
+                System.out.printf("Tháng %d có 30 ngày",n);
+                break;
+            case 7:
+                System.out.printf("Tháng %d có 31 ngày",n);
+                break;
+            case 8:
+                System.out.printf("Tháng %d có 31 ngày",n);
+                break;
+            case 9:
+                System.out.printf("Tháng %d có 30 ngày",n);
+                break;
+            case 10:
+                System.out.printf("Tháng %d có 31 ngày",n);
+                break;
+            case 11:
+                System.out.printf("Tháng %d có 30 ngày",n);
+                break;
+            case 12:
+                System.out.printf("Tháng %d có 31 ngày",n);
+                break;
+            default: System.out.printf("Tháng không hợp lệ");
+        }
+
     }
 }
