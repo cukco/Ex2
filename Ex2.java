c93473ad10f584b23178eac2d505156590c55f6b
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-01 18:17:36 +0700

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,47 +1,24 @@
 import java.util.Scanner;
-public class Ex22 {
+public class Ex42 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        switch (n){
-            case 1:
-                System.out.printf("Tháng %d có 31 ngày",n);
-                break;
-            case 2:
-                System.out.printf("Tháng %d có 28 hoặc 29 ngày",n);
-                break;
-            case 3:
-                System.out.printf("Tháng %d có 31 ngày",n);
-                break;
-            case 4:
-                System.out.printf("Tháng %d có 30 ngày",n);
-                break;
-            case 5:
-                System.out.printf("Tháng %d có 31 ngày",n);
-                break;
-            case 6:
-                System.out.printf("Tháng %d có 30 ngày",n);
-                break;
-            case 7:
-                System.out.printf("Tháng %d có 31 ngày",n);
-                break;
-            case 8:
-                System.out.printf("Tháng %d có 31 ngày",n);
-                break;
-            case 9:
-                System.out.printf("Tháng %d có 30 ngày",n);
-                break;
-            case 10:
-                System.out.printf("Tháng %d có 31 ngày",n);
-                break;
-            case 11:
-                System.out.printf("Tháng %d có 30 ngày",n);
-                break;
-            case 12:
-                System.out.printf("Tháng %d có 31 ngày",n);
-                break;
-            default: System.out.printf("Tháng không hợp lệ");
+        System.out.print("Nhập số hàng của mảng: ");
+        int rows = sc.nextInt();
+        System.out.print("Nhập số cột của mảng: ");
+        int columns = sc.nextInt();
+        int[][] arr= new int[rows][columns];
+        int odd=0;
+        int even=0;
+        System.out.println("Nhập các phần tử của mảng:");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                System.out.printf("Phần tử [%d][%d]: ",i,j);
+                arr[i][j]=sc.nextInt();
+                if(arr[i][j]%2==0){
+                    odd+=arr[i][j];
+                }else even+=arr[i][j];
+            }
         }
-
+        System.out.printf("Tổng các số chẵn: %d\nTổng các số lẻ: %d",odd,even);
     }
 }
