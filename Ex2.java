1c2156bedf0f4136bdc522f290af112a03e3c2ad
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-06 15:01:45 +0700

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
-public class Ex42 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập số hàng của mảng: ");
-        int rows = sc.nextInt();
-        System.out.print("Nhập số cột của mảng: ");
-        int columns = sc.nextInt();
-        int[][] arr= new int[rows][columns];
-        int odd=0;
-        int even=0;
-        System.out.println("Nhập các phần tử của mảng:");
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
-                System.out.printf("Phần tử [%d][%d]: ",i,j);
-                arr[i][j]=sc.nextInt();
-                if(arr[i][j]%2==0){
-                    odd+=arr[i][j];
-                }else even+=arr[i][j];
-            }
+
+public class Ex52 {
+    public static void main(String[] args){
+        long start, end;
+        String str="";
+        start=System.currentTimeMillis();
+        for(int i=0;i<1000;i++){
+            str+="a";
         }
-        System.out.printf("Tổng các số chẵn: %d\nTổng các số lẻ: %d",odd,even);
+        end=System.currentTimeMillis();
+        System.out.println("Time taken by String: "+ (end-start)+"ms");
+
+        StringBuilder strb=new StringBuilder();
+        start=System.currentTimeMillis();
+        strb.append("a".repeat(1000));
+        end=System.currentTimeMillis();
+        System.out.println("Time taken by StringBuilder: "+ (end-start)+"ms");
+
+        StringBuffer stringBuffer=new StringBuffer();
+        start=System.currentTimeMillis();
+        stringBuffer.append("a".repeat(1000));
+        end=System.currentTimeMillis();
+        System.out.println("Time taken by StringBuffer: "+ (end-start)+"ms");
+
+
+
     }
 }
