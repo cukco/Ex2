5bc14e7403b9c3781f9bb45d338285f4a2c0214f
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-08 17:11:49 +0700

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,28 +1,16 @@
-
-public class Ex52 {
+class Book{
+    String title;
+    String author;
+    double price;
+    public Book(String title,String author,double price){
+        this.title=title;
+        this.author=author;
+        this.price=price;
+    }
+}
+public class Ex72 {
     public static void main(String[] args){
-        long start, end;
-        String str="";
-        start=System.currentTimeMillis();
-        for(int i=0;i<1000;i++){
-            str+="a";
-        }
-        end=System.currentTimeMillis();
-        System.out.println("Time taken by String: "+ (end-start)+"ms");
-
-        StringBuilder strb=new StringBuilder();
-        start=System.currentTimeMillis();
-        strb.append("a".repeat(1000));
-        end=System.currentTimeMillis();
-        System.out.println("Time taken by StringBuilder: "+ (end-start)+"ms");
-
-        StringBuffer stringBuffer=new StringBuffer();
-        start=System.currentTimeMillis();
-        stringBuffer.append("a".repeat(1000));
-        end=System.currentTimeMillis();
-        System.out.println("Time taken by StringBuffer: "+ (end-start)+"ms");
-
-
-
+        Book me=new Book("Đất rừng phương Nam","Đoàn Giỏi",20);
+        System.out.print("Title: "+me.title+",Author: "+me.author+",Price: "+me.price);
     }
 }
