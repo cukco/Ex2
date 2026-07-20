bb133462080f173b35213997d7f863beaefebe92
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-20 12:29:58 +0700

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,36 +1,62 @@
-import java.lang.Math;
-class Shape{
-    public double area(){
-        return 0;
-    }
+import java.util.ArrayList;
+import java.util.List;
 
-}
-class  Rectangle extends Shape{
-    private double width;
-    private double height;
-    public Rectangle( double width,double height){
-        this.height=height;
-        this.width=width;
-    }
-    @Override
-    public double area() {
-        return width*height;
+abstract class Payment{
+    double amount;
+    abstract void pay();
+    void printAmount(){
+        System.out.printf("%.2f",amount);
     }
 }
-class  Circle extends Shape{
-    private double radius;
-    public Circle( double radius){
-        this.radius=radius;
+interface Refundable{
+    void refund();
+}
+class CashPayment extends Payment{
+    CashPayment(double a){
+        amount = a;
     }
-    @Override
-    public double area() {
-        return Math.PI*radius*radius;
+    void pay(){
+        System.out.printf("Paid by cash: %.2f",amount);
     }
 }
-public class Ex102 {
-    public static void main(String[] args){
-        Shape s1 = new Rectangle(2,3);
-        Shape s2 = new Circle(1);
-        System.out.printf("Diện tích hình chữ nhât: %.2f\nDiện tích hình tròn: %.2f",s1.area(),s2.area());
+class CreditCardPayment extends Payment implements Refundable{
+    CreditCardPayment(double a){
+        amount = a;
+    }
+    void pay(){
+        System.out.printf("Paid by CreditCard: %.2f",amount);
+    }
+    public void refund(){
+        System.out.printf(" Refund by CreditCard: %.2f",amount);
+    }
+}
+class EWalletPayment extends Payment implements Refundable{
+    EWalletPayment(double a){
+        amount = a;
+    }
+    void pay(){
+        System.out.printf("Pay by EWallet: %.2f",amount);
+    }
+    public void refund(){
+        System.out.printf(" Refund by EWallet: %.2f",amount);
+    }
+}
+public class Ex2 {
+    public static void main(String[] args) {
+        List<Payment> payments = new ArrayList<>();
+        payments.add(new CashPayment(12));
+        payments.add(new CreditCardPayment(12));
+        payments.add(new EWalletPayment(12));
+        for (int i=0;i<payments.size();i++) {
+            payments.get(i).pay();
+            try{
+                Refundable refundable = (Refundable) payments.get(i);
+                refundable.refund();
+            }catch(ClassCastException e){
+                System.out.println();
+                continue;
+            }
+            System.out.println();
+        }
     }
 }
